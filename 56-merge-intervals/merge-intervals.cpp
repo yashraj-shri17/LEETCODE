9a4ad9c387a6880db5ac5cdfa2e25317d276b0cd
yashraj-shri17 <158489592+yashraj-shri17@users.noreply.github.com>
2025-10-22 19:27:48 +0530

    Time: 12 ms (10.75%) | Memory: 24.6 MB (19.22%) - LeetSync

--- 56-merge-intervals/merge-intervals.cpp
+++ 56-merge-intervals/merge-intervals.cpp
@@ -6,26 +6,26 @@ class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& v) {
         sort(v.begin(),v.end(),cmp);
-        int start = v[0][0];
-        int end = v[0][1];
+        int final_start = v[0][0];
+        int final_end = v[0][1];
         vector<vector<int>>ans;
         for(int i=1;i<v.size();i++){
             int next_start = v[i][0];
             int next_end = v[i][1];
-            if(next_start<=end){
-                start = min(start,next_start);
-                end = max(end , next_end);       
+            if(next_start<=final_end){
+                final_start = min(final_start,next_start);
+                final_end = max(final_end , next_end);       
             }
             else{
                 vector<int>temp;
-                temp.push_back(start);
-                temp.push_back(end);
+                temp.push_back(final_start);
+                temp.push_back(final_end);
                 ans.push_back(temp);
-                start = v[i][0];
-                end = v[i][1];
+                final_start = v[i][0];
+                final_end = v[i][1];
             }
         }
-        ans.push_back({start, end});
+        ans.push_back({final_start, final_end});
         return ans ;
     }
 };
