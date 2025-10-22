3af8fa40ed85efdddc9b32e247b810e27c681b40
yashraj-shri17 <158489592+yashraj-shri17@users.noreply.github.com>
2025-10-22 16:31:49 +0530

    Time: 0 ms (100.00%) | Memory: 12.3 MB (39.48%) - LeetSync

---
+++ 797-rabbits-in-forest/rabbits-in-forest.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int numRabbits(vector<int>& arr) {
+        unordered_map<int,int>mp;
+        int ans = 0;
+        for(int i=0;i<arr.size();i++){
+            if(mp.find(arr[i]+1)!=mp.end()){
+                // found the key
+                mp[arr[i]+1]++;
+            }
+            else{
+                // not found the key
+                mp[arr[i]+1]++;
+                ans = ans + arr[i]+1;
+            }
+            int val = mp[arr[i]+1];
+            int key = arr[i]+1;
+            if(key==val){
+                mp.erase(key);
+            }
+        }
+        return ans;
+    }
+};
