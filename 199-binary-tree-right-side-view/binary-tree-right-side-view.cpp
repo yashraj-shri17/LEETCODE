77047718c7b7a9b73d2a18b555b3ea2bdcbcf243
yashraj-shri17 <158489592+yashraj-shri17@users.noreply.github.com>
2025-10-28 17:19:55 +0530

    Time: 0 ms (100.00%) | Memory: 14.7 MB (99.56%) - LeetSync

--- 199-binary-tree-right-side-view/binary-tree-right-side-view.cpp
+++ 199-binary-tree-right-side-view/binary-tree-right-side-view.cpp
@@ -9,29 +9,30 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+ // Levels FUCNTION
 class Solution {
 public:
-    int level_cal(TreeNode* root){
+    int levels(TreeNode* root){
         if(root==NULL) return 0;
-        return 1 + max(level_cal(root->left),level_cal(root->right));
+        return 1 + max(levels(root->left),levels(root->right));
     }
-    void helper(TreeNode* root , int curr , int level , vector<int>& ans){
+    void helper(TreeNode* root , int curr , int level,vector<int>& ans){
         if(root==NULL) return;
         if(curr == level){
-            ans[curr-1] = root->val;
+            ans[curr] = root->val;
             return;
         }
-        helper(root->left , curr+1 , level ,ans);
-        helper(root->right , curr+1 , level , ans);
+        helper(root->left , curr+1 , level,ans);
+        helper(root->right , curr+1 , level,ans);
     }
-    void level_order(TreeNode* root , vector<int>& ans){
-        for(int i = 1 ; i <= ans.size() ; i++){
-            helper(root , 1 , i , ans);
+    void level_order(TreeNode* root, vector<int>& ans){
+        int n_levels = ans.size();
+        for(int i = 0 ; i < n_levels ; i++){
+            helper(root , 0 , i,ans);
         }
     }
     vector<int> rightSideView(TreeNode* root) {
-        int levels = level_cal(root); 
-        vector<int>ans(levels,0);
+        vector<int>ans(levels(root),0);
         level_order(root , ans);
         return ans;
     }
