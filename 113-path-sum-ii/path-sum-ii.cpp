b8739b0a08aa7d1a8229bb89183a4a1512bfe4a7
yashraj-shri17 <158489592+yashraj-shri17@users.noreply.github.com>
2025-10-28 18:01:53 +0530

    Time: 10 ms (33.62%) | Memory: 38 MB (27.10%) - LeetSync

---
+++ 113-path-sum-ii/path-sum-ii.cpp
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void helper(TreeNode* root , vector<int>v , vector<vector<int>> & ans , int tsum){
+        if(root==NULL) return;
+        if(root->left==NULL && root->right==NULL){
+            if(tsum==root->val){
+                v.push_back(tsum);
+                ans.push_back(v);
+            }
+            return;
+        }
+        v.push_back(root->val);
+        helper(root->left,v,ans,tsum-(root->val));
+        helper(root->right,v,ans,tsum-(root->val));
+    }
+    vector<vector<int>> pathSum(TreeNode* root, int targetSum) {
+        vector<vector<int>> ans;
+        vector<int>v;
+        helper(root , v , ans , targetSum);
+        return ans;
+    }   
+};
