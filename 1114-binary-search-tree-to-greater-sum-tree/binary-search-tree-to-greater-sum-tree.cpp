4f5bd97388097128daadb54affdd8a76919e7110
yashraj-shri17 <158489592+yashraj-shri17@users.noreply.github.com>
2025-10-29 12:16:26 +0530

    Time: 0 ms (100.00%) | Memory: 11.5 MB (16.61%) - LeetSync

--- 1114-binary-search-tree-to-greater-sum-tree/binary-search-tree-to-greater-sum-tree.cpp
+++ 1114-binary-search-tree-to-greater-sum-tree/binary-search-tree-to-greater-sum-tree.cpp
@@ -3,10 +3,8 @@ public:
     void helper(TreeNode* root , int& sum){
         if(root==NULL) return;
         helper(root->right , sum);
-        // kaam karoge
         root->val =  root->val + sum;
         sum = root->val;
-        //
         helper(root->left , sum);
     }
     TreeNode* bstToGst(TreeNode* root) {
