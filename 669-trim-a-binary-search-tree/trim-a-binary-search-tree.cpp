890d960ed3816b00744a88c3da6b725226d64a76
yashraj-shri17 <158489592+yashraj-shri17@users.noreply.github.com>
2025-11-02 10:07:45 +0530

    Time: 0 ms (100.00%) | Memory: 24.9 MB (26.58%) - LeetSync

---
+++ 669-trim-a-binary-search-tree/trim-a-binary-search-tree.cpp
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void helper(TreeNode* root, int low, int high){
+        if(root==NULL) return ;
+        while(root->left!=NULL){
+            if(root->left->val < low) root->left = root->left->right;
+            else if (root->left->val > high) root->left = root->left->left;
+            else break;
+        }
+        while(root->right!=NULL){
+            if(root->right->val > high) root->right = root->right->left;
+            else if (root->right->val < low) root->right = root->right->right;
+            else break;
+        }
+        helper(root->left , low , high);
+        helper(root->right , low , high);
+    }
+    TreeNode* trimBST(TreeNode* root, int low, int high) {
+        TreeNode* dummy = new TreeNode(INT_MAX);
+        dummy->left = root;
+        helper(dummy , low , high);
+        return dummy->left;
+    }
+};
