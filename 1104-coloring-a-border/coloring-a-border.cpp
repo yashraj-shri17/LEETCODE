aa220ec623d84db9efa432fa0fb8041b9387e768
yashraj-shri17 <158489592+yashraj-shri17@users.noreply.github.com>
2025-11-06 11:56:38 +0530

    Time: 1 ms (41.19%) | Memory: 18.7 MB (9.96%) - LeetSync

---
+++ 1104-coloring-a-border/coloring-a-border.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    void dfs(vector<vector<int>>& grid , int currRow , int currCol , int actual_color ,int new_color , vector<pair<int,int>>& boundary_cells , vector<vector<int>>& visited){
+        int n = grid.size();
+        int m = grid[0].size();
+        if(currRow<0 || currCol<0 || currRow>=n || currCol>=m || grid[currRow][currCol]!=actual_color || visited[currRow][currCol]) return;
+        
+        visited[currRow][currCol] = 1;
+
+        // check boundary condition safely
+        if(currRow == 0 || currRow==n-1 || currCol==0 || currCol==m-1 ||
+           grid[currRow][currCol]!=grid[currRow+1][currCol] ||
+           grid[currRow][currCol]!=grid[currRow-1][currCol] ||
+           grid[currRow][currCol]!=grid[currRow][currCol+1] ||
+           grid[currRow][currCol]!=grid[currRow][currCol-1]){
+            boundary_cells.push_back({currRow , currCol});
+        }
+
+        dfs(grid , currRow+1 , currCol , actual_color , new_color , boundary_cells , visited);
+        dfs(grid , currRow-1 , currCol , actual_color , new_color , boundary_cells , visited);
+        dfs(grid , currRow , currCol-1 , actual_color , new_color , boundary_cells , visited);
+        dfs(grid , currRow , currCol+1 , actual_color , new_color , boundary_cells , visited);
+    }
+
+    vector<vector<int>> colorBorder(vector<vector<int>>& grid, int row, int col, int color) {
+        if(grid[row][col]==color) return grid;
+        vector<pair<int,int>> boundary_cells;
+        vector<vector<int>> visited(grid.size(), vector<int>(grid[0].size(), 0));
+        dfs(grid , row , col, grid[row][col] , color, boundary_cells , visited);
+        for(int i = 0 ; i < boundary_cells.size() ; i++){
+            int r = boundary_cells[i].first;
+            int c = boundary_cells[i].second;
+            grid[r][c] = color;
+        }
+        return grid; 
+    }
+};
