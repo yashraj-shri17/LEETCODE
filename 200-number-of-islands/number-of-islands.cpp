14f758f0923c548a9ee932c2387b38db2d3c2a5c
yashraj-shri17 <158489592+yashraj-shri17@users.noreply.github.com>
2025-11-08 14:52:38 +0530

    Time: 19 ms (95.98%) | Memory: 16.2 MB (78.77%) - LeetSync

--- 200-number-of-islands/number-of-islands.cpp
+++ 200-number-of-islands/number-of-islands.cpp
@@ -1,11 +1,8 @@
 class Solution {
 public:
     void helper(vector<vector<char>>& grid , int i , int j){
-        if(i<0 || i>=grid.size() || j<0 || j>=grid[0].size()) return;
-        if(grid[i][j] != '1') return;
-
+        if(i<0 || i>=grid.size() || j<0 || j>=grid[0].size() || grid[i][j] != '1') return;
         grid[i][j] = '0';
-
         helper(grid , i+1 , j);
         helper(grid , i-1 , j);
         helper(grid , i , j+1);
@@ -14,7 +11,6 @@ public:
 
     int numIslands(vector<vector<char>>& grid) {
         int result = 0;
-
         for(int i=0;i<grid.size();i++){
             for(int j=0;j<grid[0].size();j++){
                 if(grid[i][j]=='1'){
@@ -23,7 +19,6 @@ public:
                 }
             }
         }
-
         return result;
     }
 };
