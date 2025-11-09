d84d79d5a7098ea066a119ad37bdab1ca68e30cc
yashraj-shri17 <158489592+yashraj-shri17@users.noreply.github.com>
2025-11-09 09:11:07 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (97.15%) - LeetSync

--- 2288-count-operations-to-obtain-zero/count-operations-to-obtain-zero.cpp
+++ 2288-count-operations-to-obtain-zero/count-operations-to-obtain-zero.cpp
@@ -2,13 +2,9 @@ class Solution {
 public:
     int countOperations(int num1, int num2) {
         int ans = 0;
-        while(num1!=0 && num2!=0){
-            if(num1>=num2){
-                num1 = num1 - num2;
-            }
-            else{
-              num2 = num2 - num1;  
-            }
+        while(num1 && num2){
+            if(num1>=num2) num1 = num1 - num2;
+            else num2 = num2 - num1;  
             ans++;
         }
         return ans;
