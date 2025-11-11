792e5e7ab4f4f709ea72459d7bdbaf45b87ab7a9
yashraj-shri17 <158489592+yashraj-shri17@users.noreply.github.com>
2025-11-11 09:44:12 +0530

    Time: 6 ms (37.04%) | Memory: 31 MB (6.27%) - LeetSync

--- 4115-minimum-distance-between-three-equal-elements-i/minimum-distance-between-three-equal-elements-i.cpp
+++ 4115-minimum-distance-between-three-equal-elements-i/minimum-distance-between-three-equal-elements-i.cpp
@@ -1,26 +1,21 @@
 class Solution {
 public:
     int minimumDistance(vector<int>& nums) {
-        unordered_map<int,vector<int>>mp;
-        for(int i = 0 ; i < nums.size() ; i++){
-            mp[nums[i]].push_back(i); // index
-        }
+        unordered_map<int, vector<int>> mp;
+        for (int i = 0; i < nums.size(); i++) mp[nums[i]].push_back(i);
         int mini = INT_MAX;
-        for(auto x : mp){
-            int ele = x.first;
+        for (auto x : mp) { 
             vector<int> ele_vector = x.second;
-            if(ele_vector.size()>=3){
-                int temp = ele_vector.size() - 3;
-                for(int i = 0 ; i <= temp ; i++){
-                    int first = ele_vector[i];
-                    int second = ele_vector[i+1];
-                    int third = ele_vector[i+2];
-                    int ans = abs(first-second) + abs(second-third) + abs(third-first);
-                    mini = min(mini , ans);
-                }       
+            if (ele_vector.size() >= 3) {
+                for (int i = 0; i <= ele_vector.size() - 3; i++) { 
+                    int val = abs(ele_vector[i] - ele_vector[i + 1]) 
+                            + abs(ele_vector[i + 1] - ele_vector[i + 2]) 
+                            + abs(ele_vector[i + 2] - ele_vector[i]);
+                    mini = min(mini, val);
+                }
             }
         }
-        if(mini ==INT_MAX) return -1;
+        if (mini == INT_MAX) return -1;
         return mini;
     }
 };
