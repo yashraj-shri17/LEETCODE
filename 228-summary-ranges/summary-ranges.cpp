34674ae46925823261201ed8085a24cfea6ec474
yashraj-shri17 <158489592+yashraj-shri17@users.noreply.github.com>
2025-11-13 13:02:39 +0530

    Time: 0 ms (100.00%) | Memory: 9.2 MB (73.08%) - LeetSync

--- 228-summary-ranges/summary-ranges.cpp
+++ 228-summary-ranges/summary-ranges.cpp
@@ -3,26 +3,18 @@ public:
     vector<string> summaryRanges(vector<int>& nums) {
         vector<string> res;
         if(nums.empty()) return res;
-
-        int start = nums[0]; // start of current range
-
+        int start = nums[0];
         for(int i = 1; i < nums.size(); i++){
-            // if not consecutive
             if(nums[i] != nums[i-1] + 1){
                 if(start == nums[i-1]) 
                     res.push_back(to_string(start));
                 else 
                     res.push_back(to_string(start) + "->" + to_string(nums[i-1]));
-                start = nums[i]; // reset start for next range
+                start = nums[i];
             }
         }
-
-        // handle last range
-        if(start == nums.back())
-            res.push_back(to_string(start));
-        else
-            res.push_back(to_string(start) + "->" + to_string(nums.back()));
-
+        if(start == nums.back()) res.push_back(to_string(start));
+        else res.push_back(to_string(start) + "->" + to_string(nums.back()));
         return res;
     }
 };
