5ca811f4778f970e5cb4ffb3420659768be85aa3
yashraj-shri17 <158489592+yashraj-shri17@users.noreply.github.com>
2025-11-14 12:52:03 +0530

    Time: 16 ms (82.76%) | Memory: 93.4 MB (15.67%) - LeetSync

---
+++ 2625-increment-submatrices-by-one/increment-submatrices-by-one.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<vector<int>> rangeAddQueries(int n, vector<vector<int>>& queries) {
+        vector<vector<int>> diff(n + 1, vector<int>(n + 1, 0));
+        for (auto& qu : queries) {
+            int r1 = qu[0], c1 = qu[1], r2 = qu[2], c2 = qu[3];
+            diff[r1][c1]++;
+            diff[r2 + 1][c1]--;
+            diff[r1][c2 + 1]--;
+            diff[r2 + 1][c2 + 1]++;
+        }
+        vector<vector<int>> mat(n, vector<int>(n, 0));
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                int above = i > 0 ? mat[i - 1][j] : 0;
+                int left = j > 0 ? mat[i][j - 1] : 0;
+                int diag = (i > 0 && j > 0) ? mat[i - 1][j - 1] : 0;
+                mat[i][j] = diff[i][j] + above + left - diag;
+            }
+        }
+        return mat;
+    }
+};
