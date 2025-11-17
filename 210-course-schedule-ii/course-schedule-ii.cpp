d447e05134eebee9933f8c71587c57ba130d18eb
yashraj-shri17 <158489592+yashraj-shri17@users.noreply.github.com>
2025-11-17 14:50:46 +0530

    Time: 5 ms (44.67%) | Memory: 19.1 MB (18.66%) - LeetSync

---
+++ 210-course-schedule-ii/course-schedule-ii.cpp
@@ -0,0 +1,50 @@
+class Solution {
+public:
+    vector<list<int>>graph;
+    vector<int>res_ans;
+    int n;
+    void add_edge(int from , int to , bool bi_dir = true){
+        graph[from].push_back(to);
+        if(bi_dir) graph[to].push_back(from);
+    }
+    void kahns_algo(){
+        vector<int>indegree(n,0);
+        for(int i = 0 ; i < n ; i++){
+            for(auto neighbours : graph[i]){
+                indegree[neighbours]++;
+            }
+        }
+        queue<int>qu;
+        unordered_set<int>vis;
+        for(int i = 0 ; i < n ; i++){
+            if(indegree[i]==0){
+                qu.push(i);
+                vis.insert(i);
+            }
+        }
+        while(qu.size()>0){
+            int front = qu.front();
+            res_ans.push_back(front);
+            qu.pop();
+            for(auto neighbour : graph[front]){
+                indegree[neighbour]--;
+                if(indegree[neighbour]==0){
+                    qu.push(neighbour);
+                    vis.insert(neighbour);
+                }
+            }
+        }
+    }
+    vector<int> findOrder(int numCourses, vector<vector<int>>& prereq) {
+        n = numCourses;
+        graph.resize(n,list<int>());
+        for(int i = 0 ; i < prereq.size() ; i++){
+            int from = prereq[i][1];
+            int to = prereq[i][0];
+            add_edge(from , to , false);
+        }
+        kahns_algo();
+        if(res_ans.size()<numCourses) return {};
+        return res_ans;
+    }
+};
