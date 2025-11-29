9525b66766f541911f7c60603d217b929669e878
yashraj-shri17 <158489592+yashraj-shri17@users.noreply.github.com>
2025-11-29 17:02:33 +0530

    Time: 274 ms (51.47%) | Memory: 62.7 MB (54.37%) - LeetSync

--- 1706-min-cost-to-connect-all-points/min-cost-to-connect-all-points.cpp
+++ 1706-min-cost-to-connect-all-points/min-cost-to-connect-all-points.cpp
@@ -1,22 +1,16 @@
 class Solution {
 public:
-    vector<int>parents;
-    vector<int>Rank;
+    vector<int>parents, Rank;
     struct Edge{
-        int a;
-        int b;
-        int val;
+        int a ,b ,val;
     };
     vector<Edge> coord_connect;
-
     int findp(int child){
         if(parents[child]==child) return child;
         return parents[child] = findp(parents[child]);
     }
-
     bool unite(int x , int y){
-        int a = findp(x);
-        int b = findp(y);
+        int a = findp(x) , b = findp(y);
         if(a!=b){
             if(Rank[a]<Rank[b]) swap(a,b);
             parents[b] = a;
@@ -25,33 +19,24 @@ public:
         }
         return false;      // CYCLE
     }
-
     int kruskals(){
         sort(coord_connect.begin(), coord_connect.end(),
          [](const Edge &x, const Edge &y){
              return x.val < y.val;
          });
-
-        int n = coord_connect.size();
         int minCost = 0;
-
-        for(int i = 0; i < n; i++){
+        for(int i = 0; i < coord_connect.size(); i++){
             Edge curr = coord_connect[i];
-            if(unite(curr.a, curr.b)){   // If merged successfully, add cost
-                minCost += curr.val;
-            }
+            if(unite(curr.a, curr.b)) minCost += curr.val; // if merged successfully
         }
         return minCost;
     }
 
     int minCostConnectPoints(vector<vector<int>>& points) {
         int n = points.size();
-
         parents.resize(n);
         Rank.resize(n,0);
         for(int i = 0; i < n; i++) parents[i] = i;
-
-        /// create all pair edges
         for(int i = 0 ; i < n ; i++){
             for(int j = i+1 ; j < n ; j++){
                 coord_connect.push_back({i, j,
@@ -59,7 +44,6 @@ public:
                 });
             }
         }
-
         return kruskals();
     }
 };
