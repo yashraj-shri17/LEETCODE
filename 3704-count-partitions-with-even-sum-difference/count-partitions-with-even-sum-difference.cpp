4a9e8a4e9e4f6dff2bff1f230cc0d5234029d339
yashraj-shri17 <158489592+yashraj-shri17@users.noreply.github.com>
2025-12-05 12:34:30 +0530

    Time: 0 ms (100.00%) | Memory: 22.5 MB (53.79%) - LeetSync

--- 3704-count-partitions-with-even-sum-difference/count-partitions-with-even-sum-difference.cpp
+++ 3704-count-partitions-with-even-sum-difference/count-partitions-with-even-sum-difference.cpp
@@ -1,17 +1,13 @@
 class Solution {
 public:
     int countPartitions(vector<int>& nums) {
-        int tsum  = 0 ;
-        int cnt = 0;
-        for(auto x : nums) tsum+=x; // tsum-->>36
+        int tsum  = 0 , cnt = 0;
+        for(auto x : nums) tsum+=x;
         int leftsum = 0 , rightsum = 0;
         for(int i = 0 ; i < nums.size()-1 ; i++){
-            leftsum+=nums[i]; // leftsum->10-->20
-            rightsum = tsum - leftsum; // rightsum-->26-->16
-            if(abs(leftsum-rightsum)%2==0){
-                cout<<abs(leftsum-rightsum)<<" ";
-                cnt++;
-            } //cnt-->1
+            leftsum+=nums[i]; 
+            rightsum = tsum - leftsum; 
+            if(abs(leftsum-rightsum)%2==0) cnt++;
         }
         return cnt;
     }
