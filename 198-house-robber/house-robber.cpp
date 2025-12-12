42300c5424061aeed56400e3a75d8032d276c388
yashraj-shri17 <158489592+yashraj-shri17@users.noreply.github.com>
2025-12-12 22:29:35 +0530

    Time: 0 ms (100.00%) | Memory: 10.1 MB (89.06%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -12,10 +12,24 @@ public:
         if(dp[i]!=-1) return dp[i];
         return dp[i] = max(nums[i] + ftd(nums , i+2) , ftd(nums , i+1));
     }
+
+    int fbu(vector<int>& nums){
+        int n = nums.size();
+        if(n==1) return nums[n-1];
+        if(n==2) return max(nums[n-2] , nums[n-2+1]);
+        dp.clear();
+        dp.resize(105,-1);
+        dp[n-1] = nums[n-1];
+        dp[n-2] = max(nums[n-2] , nums[n-2+1]);
+        for(int i = n-3 ; i>=0 ; i--){
+            dp[i] = max(nums[i] + dp[i+2] , dp[i+1]);
+        }
+        return dp[0];
+    }
     int rob(vector<int>& nums) {
         dp.clear();
         dp.resize(110,-1);
         //return rec(nums, 0);
-        return ftd(nums , 0);
+        return fbu(nums);
     }
 };
