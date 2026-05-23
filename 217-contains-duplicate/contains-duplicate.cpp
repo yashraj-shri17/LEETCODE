640e8e0dca2eca3c36e9a3b1def3809be552e062
yashraj-shri17 <158489592+yashraj-shri17@users.noreply.github.com>
2026-05-23 13:19:44 +0530

    Time: 54 ms (34.35%) | Memory: 93 MB (8.27%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.cpp
+++ 217-contains-duplicate/contains-duplicate.cpp
@@ -4,9 +4,7 @@ public:
         unordered_map<int,int>mp;
         for(auto x : nums) mp[x]++;
         for(auto x : mp){
-            if(x.second>1){
-                return true;
-            }
+            if(x.second>1) return true;
         }
         return false;
     }
